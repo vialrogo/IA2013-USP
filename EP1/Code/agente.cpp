f9ca1ed03fdf19cf996ae324dc4797ba57fae67a
Monna <monnacs@gmail.com>
2013-09-25 11:33:10 -0300

    Search functions skeleton implemented

--- EP1/Code/agente.cpp
+++ EP1/Code/agente.cpp
@@ -3,12 +3,22 @@
 ** Disciplina: Inteligência Artificial
 ** Exercício-Programa 1 (Busca Informada)
 ** Alunos: 
-** 	Monna Cleide Santos - n. USP 8477852
-** 	Victor Alberto Romero - n. USP XXXXXXX
+** 	    Monna Cleide Santos - n. USP 8477852
+** 	    Victor Alberto Romero - n. USP XXXXXXX
 **
 ** 27 de setembro de 2013
 */
 
-// Função Agente
+// Agent Function
 
+string widthSearch (Char** Matrix, int matrixSize, int nuggetCount) {
+	//This function implements...
+}
 
+string iterativeDepthSearch (Char** Matrix, int matrixSize, int nuggetCount) {
+	//This function implements...
+}
+
+string aStarSearch (Char** Matrix, int matrixSize, int nuggetCount) {
+    //This function implements... 
+}
--- EP1/Code/ambiente.cpp
+++ EP1/Code/ambiente.cpp
@@ -3,8 +3,8 @@
 ** Disciplina: Inteligência Artificial
 ** Exercício-Programa 1 (Busca Informada)
 ** Alunos: 
-** 	Monna Cleide Santos - n. USP 8477852
-** 	Victor Alberto Romero - n. USP XXXXXXX
+** 		Monna Cleide Santos - n. USP 8477852
+** 		Victor Alberto Romero - n. USP XXXXXXX
 **
 ** 27 de setembro de 2013
 */
