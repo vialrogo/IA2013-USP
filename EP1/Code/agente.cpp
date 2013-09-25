5e50bf2fe9eefde0f94a1585d38529aa368a0930
Monna <monnacs@gmail.com>
2013-09-24 23:03:21 -0300

    New 'Code' folder added with the basic files

---
+++ EP1/Code/agente.cpp
@@ -0,0 +1,12 @@
+/* IME-USP
+** Mestrado em Ciência da Computação
+** Disciplina: Inteligência Atificial
+** Exercício-Programa 1 (Busca Informada)
+** Alunos: 
+** 	Monna Cleide Santos - n. USP 8477852
+** 	Victor Alberto Romero - n. USP XXXXXXX
+**
+** 27 de setembro de 2013
+*/
+
+// Função Agente
