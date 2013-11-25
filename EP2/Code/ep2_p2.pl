9b7351b7e3f4c517297e0e28a85aa9dc6383142f
Monna <monnacs@gmail.com>
2013-11-25 09:44:36 -0200

    Splitting part 2

---
+++ EP2/Code/ep2_p2.pl
@@ -0,0 +1,14 @@
+%Prolog
+
+% IME-USP
+% Mestrado em Ciência da Computação
+% Disciplina: Inteligência Artificial
+% Exercício-Programa 2 (DCC-IME)
+% Alunos: 
+% 	    Monna Cleide Santos   - n. USP 8477852
+% 	    Victor Alberto Romero - n. USP 8405274
+%
+% 25 de novembro de 2013
+%
+
+monta_lista(L) :- readln(L).
