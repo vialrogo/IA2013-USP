1c7c7fdbd02fdc24c9bf3da7808c78136bfe9bca
Victor Alberto Romero <vialrogo@gmail.com>
2013-11-25 15:01:48 -0200

    Lnhas simples da gramatica prontas, falta a linha com duas coisas

--- EP2/Code/ep2_1.pl
+++ EP2/Code/ep2_1.pl
@@ -16,11 +16,43 @@ adjetivo(professora).
 verbo(da).
 verbo(eh).
 
-%Ponto
+%Pontuação
 ponto('.').
-
-%Pergunta
 pergunta(?).
+
+%conjunção
+conjuncao(e).
+
+%Adverbio
+adverbio(aas).
+
+%Dias
+dia(segundas).
+dia(tercas).
+dia(quartas).
+dia(quintas).
+dia(sextas).
+dia(sabados).
+dia(domingos).
+
+%Predicado que confirma se alguma coisa é uma lista de dias com conjunções
+lista_dias([]).
+lista_dias([H | [] ]) :- dia(H), !.
+lista_dias([D,C|R])   :- dia(D), conjuncao(C), lista_dias(R).
+
+%Converte um dia num número
+convertDia(segundas, 2).
+convertDia(tercas, 3). 
+convertDia(quartas, 4).
+convertDia(quintas, 5).
+convertDia(sextas, 6).
+convertDia(sabados, 7).
+convertDia(domingos, 1).
+
+%Converte uma lista de dias, numa lista de números e apaga as conuunções
+convertLD([],[]).
+convertLD([H | []], [Hc | []]) :- convertDia(H, Hc), !.
+convertLD( [H, _ |T], [Hc|Tc]) :- convertDia(H, Hc), convertLD(T, Tc).
 
 %Banco de conhecimento
 :- dynamic professor/1.
@@ -30,14 +62,21 @@ pergunta(?).
 :- dynamic eh/2.
 
 %Inserções
-inserir(X,[Ar,Ad,Pr,Vr,Di]) :- artigo(Ar),adjetivo(Ad),verbo(Vr),
-                                  P =.. [Ad,Pr], assertz(P),
-                                  assertz(disciplina(Di)),
-                                  X =.. [Vr, Pr, Di], assertz(X).
+inserir(X,[Ar,Ad,Pr,Vr,Di]) :-  artigo(Ar),adjetivo(Ad),verbo(Vr),
+                                P =.. [Ad,Pr], assertz(P),
+                                assertz(disciplina(Di)),
+                                X =.. [Vr, Pr, Di], assertz(X).
+
+inserir(X,[Di,Vr,Adv|Ld]) :- verbo(Vr),adverbio(Adv),lista_dias(Ld),
+                             assertz(disciplina(Di)),
+                             convertLD(Ld,Ldn),
+                             X =.. [Vr, Di, Ldn], assertz(X).
 
 %Sentenca
-sentenca(X,Frase,G) :- reverse(Frase,[H|T]),ponto(H),reverse(T,F),inserir(X,F). 
-sentenca(X,Frase,G) :- reverse(Frase,[H|T]),pergunta(H),reverse(T,F),responder(X,F). 
+sentenca(X,Frase,_) :-  reverse(Frase,[H|T]),ponto(H),reverse(T,F),
+                        inserir(X,F), !. 
+sentenca(X,Frase,_) :-  reverse(Frase,[H|T]),pergunta(H),reverse(T,F),
+                        responder(X,F), !. 
 
 %Apagar tudo
 apagar :-   retractall(professor(_)),
@@ -50,11 +89,12 @@ apagar :-   retractall(professor(_)),
 %%%%%%%%%%%%%%%%%%%%%%%%% Segunda parte %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%Downcase duma lista
 downCaseList([],[]).
 downCaseList([H|T], [Hl|Tl]) :- downcase_atom(H,Hl), downCaseList(T, Tl).
 
+%Monta lista
 monta_lista(L) :- readln(X), downCaseList(X,L).
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%% Terceira parte %%%%%%%%%%%%%%%%%%%%%%%%%%%%
