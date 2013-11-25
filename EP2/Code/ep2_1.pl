8f04162eab93207e4a6e21be04d34e1531a668cf
Victor Alberto Romero <vialrogo@gmail.com>
2013-11-25 20:33:19 -0200

    Bagunça :S

--- EP2/Code/ep2_1.pl
+++ EP2/Code/ep2_1.pl
@@ -18,7 +18,7 @@ verbo(eh).
 
 %Pontuação
 ponto('.').
-pergunta(?).
+spergunta(?).
 
 %conjunção
 conjuncao(e).
@@ -82,10 +82,10 @@ inserir(X,[Ar,Ad,_,Vr,Di,Adv|Ld]) :- artigo(Ar),adjetivo(Ad),verbo(Vr),
 
 %Sentenca
 sentenca(X,Frase,_) :-  reverse(Frase,[H|T]),ponto(H),reverse(T,F),inserir(X,F). 
-sentenca(X,Frase,_) :-  reverse(Frase,[H|T]),pergunta(H),reverse(T,F),responder(X,F). 
+sentenca(_,Frase,_) :-  reverse(Frase,[H|T]),spergunta(H),reverse(T,F),responder(F). 
 
 %Apagar tudo
-apagar :-   retractall(professor(_)),
+apagar :-   retractall(professor(_)), 
             retractall(professora(_)),
             retractall(disciplina(_)),
             retractall(da(_,_)),
@@ -105,4 +105,45 @@ monta_lista(L) :- readln(X), downCaseList(X,L).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%% Terceira parte %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%Perguntas
+pergunta(quem).
+pergunta(quando).
+
+oque([o, que]).
+
+%Monta uma lista com um elemento repetido, a quantidade de elementos de outra
+%lista. Serve para writes repetidos
+montarListaS(_,[],[]).  
+montarListaS(S,[_|T],X) :- montarListaS(S,T,Y), append([S],Y,X).
+
+% Consulta: "Quem da macxxx?"
+% Resposta: "O professor yyy da macxxx."
+responder([quem, da | X]) :- disciplina(X)) -> Di is X; (oque(X) -> true; false)), 
+                             findall([Pr,Di], da(Pr,Di), L),
+                             montarListaS('O professor %w da %w\n',L,S),
+                             maplist(writef,S,L).
+
+% Consulta: "Quem da o que?"
+% Resposta: "O profesor yy1 da macxx1.
+% A professora yy2 da mac xx2.
+% ...."
+responder([quem, da, o, que]) :- findall([Pr,Di], da(Pr,Di), L),
+                                 montarListaS('O professor %w da %w\n',L,S),
+                                 maplist(writef,S,L).
+
+% Consulta: "Quando eh macxxx?"
+% Resposta: "Macxxx eh as yyy e zzz."
+responder([quando, eh, o, que]) :- findall([Di,Ho], eh(Di,Ho), L),
+                                 montarListaS('%w eh aas %w\n',L,S),
+                                 maplist(writef,S,L).
+
+% Consulta: "O que o professor xxx da?"
+% Resposta: "O professor xxx da macyyy."
+
+% Consulta: "O que a professora xxx da?"
+% Resposta: "A professora xxx da macyyy."
+
+%Conlusta ao programa
+consulta :- monta_lista(L), sentenca(_,L,_).
 
